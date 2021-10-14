CUBRID/cubrid-jdbc

commit 3807c2b6a269f0674f054abb78a826e1b9e02430

[CBRD-24063] Error occurs when Connection.close() is called multiple times in Server-side JDBC (#19)
http://jira.cubrid.org/browse/CBRD-24063

When entering SP function body in ExecuteThread, The thread has INVOKE status. and If Server-side JDBC opens Connection, ExecuteThread has CALL status and CAS enters libcas_main(). disconnect() should be called only when CAS is in CALL state to exit from libcas_main(), otherwise it should do nothing.

src/jdbc/cubrid/jdbc/jci/UServerSideConnection.java
@@ -46,7 +46,9 @@ import java.net.Socket;
 import java.util.List;
 
 public class UServerSideConnection extends UConnection {
+    /* defined in ExecuteThreadStatus.java */
     public static final int INVOKE = 2;
+    public static final int CALL = 3;
 
     private static final byte CAS_CLIENT_SERVER_SIDE_JDBC = 6;
 
@@ -190,13 +192,23 @@ public class UServerSideConnection extends UConnection {
     protected void closeInternal() {
         if (client != null) {
             stmtHandlerCache.clearStatus();
-            disconnect();
-            UJCIUtil.invoke(
-                    "com.cubrid.jsp.ExecuteThread",
-                    "setStatus",
-                    new Class[] {Integer.class},
-                    this.curThread,
-                    new Object[] {INVOKE});
+            int currentStatus =
+                    (Integer)
+                            UJCIUtil.invoke(
+                                    "com.cubrid.jsp.ExecuteThread",
+                                    "getStatus",
+                                    new Class[] {},
+                                    this.curThread,
+                                    new Object[] {});
+            if (currentStatus == CALL) {
+                disconnect();
+                UJCIUtil.invoke(
+                        "com.cubrid.jsp.ExecuteThread",
+                        "setStatus",
+                        new Class[] {Integer.class},
+                        this.curThread,
+                        new Object[] {INVOKE});
+            }
         }
     }
 

commit 3b279354f7509f594ae02ef6044fd250b011b7c2

[CBRD-24063] Fix "Cannot communicate with the broker" error (#22)
http://jira.cubrid.org/browse/CBRD-24063

This PR fixes a regression caused by 3807c2b. In ExecuteThreadStatus.DESTROY status, UServerSideConnection.close(I) also be called, and It causes "Cannot communicate with the broker" error at the end of calling Java Stored procedure.

src/jdbc/cubrid/jdbc/jci/UServerSideConnection.java
@@ -200,7 +200,7 @@ public class UServerSideConnection extends UConnection {
                                     new Class[] {},
                                     this.curThread,
                                     new Object[] {});
-            if (currentStatus == CALL) {
+            if (currentStatus != INVOKE) {
                 disconnect();
                 UJCIUtil.invoke(
                         "com.cubrid.jsp.ExecuteThread",